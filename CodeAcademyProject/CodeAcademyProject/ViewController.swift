dbbd3b051f098fc15cbbf7946a25461c3ff4f087
Arvydas Klimavicius <arvydas.klimavicius@icloud.com>
2019-11-26 19:50:58 +0200

    Create project

---
+++ CodeAcademyProject/CodeAcademyProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CodeAcademyProject
+//
+//  Created by Arvydas Klimavicius on 2019-11-26.
+//  Copyright © 2019 Arvydas Klimavicius. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
