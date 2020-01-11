c2f20734de750c2bd5b00d7cd4b5707738b1dae1
Arvydas Klimavicius <arvydas.klimavicius@icloud.com>
2020-01-11 17:15:14 +0200

    StarWarsUIStructure

---
+++ CodeAcademyProject/CodeAcademyProject/Lesson 11 StarWars/DetailsDescribableProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  DetailsDescribableProtocol.swift
+//  CodeAcademyProject
+//
+//  Created by Arvydas Klimavicius on 2020-01-11.
+//  Copyright © 2020 Arvydas Klimavicius. All rights reserved.
+//
+
+import Foundation
---
+++ CodeAcademyProject/CodeAcademyProject/Lesson 11 StarWars/StarWarsCell.swift
@@ -0,0 +1,24 @@
+//
+//  StarWarsCell.swift
+//  CodeAcademyProject
+//
+//  Created by Arvydas Klimavicius on 2020-01-10.
+//  Copyright © 2020 Arvydas Klimavicius. All rights reserved.
+//
+
+import UIKit
+
+class StarWarsCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
